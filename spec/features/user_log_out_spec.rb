5c37d5816479dfa958afa9350a0e4f672755f334
Jean Joeris <joeris.peter@gmail.com>
2016-12-19 11:55:17 -0700

    Add test for users logging out

---
+++ spec/features/user_log_out_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "user logs in", type: :feature do
+  it 'works' do
+    visit root_path
+
+    click_on ("sign up")
+
+    click_on ("Sign up")
+
+    expect(current_path).to eq(new_user_registration_path)
+
+    fill_in("user_email", with: "foo@example.com")
+    fill_in("user_password", with: "123456")
+    fill_in("user_password_confirmation", with: "123456")
+
+    click_on("Sign up")
+
+    click_on("sign out")
+
+    expect(page).to have_content("sign up")
+  end
+end
