0bca7e3b9ed3ba0d99f8dc382e4e61ad36c04c96
Jean Joeris <joeris.peter@gmail.com>
2016-12-19 17:45:40 -0700

    Add javascript for pulling in top reads

---
+++ app/assets/javascripts/fetch_top_links.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  loadTopLinks()
+})
+
+function loadTopLinks() {
+  $.getJSON('http://localhost:2000/api/v1/reads')
+  .then(function(topLinks) {
+    topLinks.forEach(renderTopLink)
+  })
+}
+
+function renderTopLink(link) {
+  $('#top_reads').append(topLinkHTML(link))
+}
+
+function topLinkHTML (link) {
+  return `<div class='link container' data-id='${link.id}' id="link-${link.id}">
+            <p class='link-title' contenteditable=true>${ link.link_title }</p>
+            <p class='link-url' contenteditable=true>${ link.link_url }</p>
+            <p class='link_read_count'># of reads - ${link.read_count}</p>
+            <p class="link_buttons">
+              <button class='delete-link'>Delete</button>
+              <button class='read-link'>I have read this</button>
+            </p>
+          </div>`
+}
